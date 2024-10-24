134664a27854f52a57dc989a1d3b6cf814e9f200
NazPotseluiko <NAZARII.POTSELUIKO@lnu.edu.ua>
2024-10-24 10:42:31 +0300

    Create lab6_1.c

---
+++ lab6/lab6_1.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <locale.h>
+
+int is_vowel(char c) {
+    c = tolower((unsigned char)c);  
+    return (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' || 
+            c == 'а' || c == 'е' || c == 'є' || c == 'и' || c == 'і' || c == 'ї' || c == 'о' || c == 'у' || c == 'ю' || c == 'я');  
+}і
+
+int is_consonant(char c) {
+    c = tolower((unsigned char)c);  
+    return ((c >= 'b' && c <= 'z' && !is_vowel(c)) ||   
+            (c >= 'б' && c <= 'щ' && !is_vowel(c) && c != 'ь' && c != 'й'));  
+}
+
+int main() {
+    setlocale(LC_ALL, ""); 
+    char sentence[256];
+    int vowel_count = 0, consonant_count = 0;
+
+    printf("Введіть речення: ");
+    fgets(sentence, sizeof(sentence), stdin);
+
+    for (int i = 0; sentence[i] != '\0'; i++) {
+        if (is_vowel(sentence[i])) {
+            vowel_count++;
+        } else if (is_consonant(sentence[i])) {
+            consonant_count++;
+        }
+    }
+
+    printf("Кількість голосних: %d\n", vowel_count);
+    printf("Кількість приголосних: %d\n", consonant_count);
+
+    return 0;
+}
