8073a99eda25ab30c9780b49c1762a570d350d42
ManachynskyiT <Taras.Manachynskyi@lnu.edu.ua>
2024-10-24 10:49:56 +0300

    Update lab6_1.c

--- lab6/lab6_1.c
+++ lab6/lab6_1.c
@@ -5,15 +5,13 @@ int main() {
     char sentence[1000];
     int vowels = 0, consonants = 0;
     
-
     printf("PLS Write Sentence: ");
     fgets(sentence, sizeof(sentence), stdin);
-    
     
     for (int i = 0; sentence[i] != '\0'; i++) {
         char ch = tolower(sentence[i]);   
         if (ch >= 'a' && ch <= 'z') {    
-            if (ch == 'a' || ch == 'e'|| ch == 'i'||  ch == 'o' || ch == 'u' || ch == 'y') {
+            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' || ch == 'y') {
                 vowels++;  
             } else {
                 consonants++; 
